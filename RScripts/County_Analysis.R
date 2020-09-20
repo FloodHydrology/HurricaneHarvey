21a7b32a3fd0ae5092c64aa1340e9b0cc4cddca1
cnjones7 <>
2020-09-20 17:32:50 -0500

    SI plots

--- RScripts/County_Analysis.R
+++ RScripts/County_Analysis.R
@@ -165,11 +165,24 @@ counties<-counties %>% dplyr::left_join(., output)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #4.1 Organize data by zip-------------------------------------------------------
 #Brazoria Samples~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-brazoria<-read_xlsx(paste0(data_dir, "All labs/All binary_v2.xlsx"), sheet = 'Brazoria', skip=1) %>% 
-  dplyr::select('...1', 'TOTAL...13', 'TOTAL...27') %>% 
-  rename(zip='...1', n_pos = 'TOTAL...13', n_samples = 'TOTAL...27') %>% 
+#Read in Brazoria county bianary data
+brazoria<-
+  read_xlsx(
+    paste0(data_dir, "All labs/All binary_v2.xlsx"), 
+    sheet = 'Brazoria', 
+    skip=1) 
+
+#Rename cols manually
+colnames(brazoria)<-c("zip_1", "AUG_1",	"SEPT_1", "OCT_1", "NOV_1",	"DEC_1",
+                      "JAN_1", "FEB_1",	"MAR_1",  "APR_1", "MAY_1", "JUN_1",	
+                      "TOTAL_1", "blank","zip_2","AUG_2",	"SEPT_2",	"OCT_2", "NOV_2", "DEC_2",
+                      "JAN_2", "FEB_2",	"MAR_2", "APR_2",	"MAY_2", "JUN_2", "TOTAL_2")
+#Tidy
+brazoria <- brazoria %>% 
+  rename(zip='zip_1', n_pos = 'TOTAL_1', n_samples = 'TOTAL_2') %>% 
   mutate(test='TC') %>% 
-  ungroup(zip) %>%  mutate(zip=as.numeric(paste(zip)))
+  #ungroup(zip) %>% 
+  mutate(zip=as.numeric(paste(zip))) 
 
 #Houston Samples~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 h_1<-read_xlsx(paste0(data_dir, "All labs/All binary_v2.xlsx"), sheet = 'Houston', skip=1) %>% 
@@ -263,10 +276,14 @@ df_county<-
   #Bind rows and select distinct rows
   bind_rows() %>% distinct()
 
+#Something weird is happening -- a [1] is showing up in the col type? 
+df_county$prop<-as.numeric(paste0(df_county$prop))
+
 #Add county info to df
 df_county<-df_county %>% 
   mutate(zip = as.numeric(paste(zip))) %>% 
-  left_join(., df_zip)
+  left_join(., df_zip) %>% 
+  drop_na()
 
 #Distribute samples accross counties [assume equal distribution of samples accross zip]
 df_county<-df_county %>% 
@@ -283,8 +300,7 @@ df_county<-df_county %>%
 #Make wider for join
 df_county<-df_county %>% 
   pivot_wider(names_from='test', 
-              values_from = c('n_pos', 'n_samples')) %>% 
-  select(-c('n_pos_NA', 'n_samples_NA'))
+              values_from = c('n_pos', 'n_samples')) 
 
 #Join to master dataframe
 counties<-counties %>% left_join(., df_county %>% dplyr::rename(NAME = county)) 
@@ -323,7 +339,9 @@ write_csv(df_county, paste0(output_dir, "county_sample_summary.csv"))
 
 #5.3 County plot----------------------------------------------------------------
 export<-counties %>% 
-  mutate(prop_inun_area = prop_inun_area*100, 
+  mutate(prop_inun_area = as.numeric(paste(prop_inun_area)),
+         prop_inun_area = prop_inun_area*100, 
+         prop_inun_wells = prop_inun_wells*100,
          prop_well_users = total_well_users/pop*100, 
          prop_pos_EC = n_pos_EC/n_samples_EC*100,
          prop_pos_TC = n_pos_TC/n_samples_TC*100) %>%  
@@ -346,17 +364,25 @@ tmap_mode("plot")
 
 #Create plots
 w1<-tm_shape(export) + 
-  tm_polygons('Well users', palette = "Greys", style = 'fixed', breaks=c(1500,3000,6000,9000,16000,110000)) +
-  tm_layout(frame=F, legend.show = T, title="A)", title.size = 14/12)  
+  tm_polygons('Well users', palette = "RdPu", style = 'fixed', breaks=c(1500,3000,6000,9000,16000,110000)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="A)", title.size = 14/12)  
 w2<-tm_shape(export) + 
-  tm_polygons('Area flooded (%)', palette = "Blues", style = 'fixed', breaks=c(0,1,5,10,25,66)) +
-  tm_layout(frame=F, legend.show = T, title="B)", title.size = 14/12)  
+  tm_polygons('Area flooded (%)', palette = "GnBu", style = 'fixed', breaks=c(0,1,5,10,25,66)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="B)", title.size = 14/12)  
 w3<-tm_shape(export) + 
   tm_polygons('Well users flooded', palette = "BuPu",style = 'fixed', breaks=c(0,10,50, 250,500,3000)) +
-  tm_layout(frame=F, legend.show = T, title="C)", title.size = 14/12)  
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="C)", title.size = 14/12)  
 w4<-tm_shape(export) + 
-  tm_polygons('Well users flooded (%)', palette = "Reds", style = 'fixed', breaks=c(0,0.01,0.02, 0.05,0.1,0.2)) +
-  tm_layout(frame=F, legend.show = T, title="D)", title.size = 14/12) 
+  tm_polygons('Well users flooded (%)', palette = "Blues", style = 'fixed', breaks=c(0,1,2, 5,10,20)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="D)", title.size = 14/12) 
 
 #plot
 tmap_arrange(w1,w2,w3,w4, ncol=2)
@@ -371,17 +397,25 @@ tmap_mode("plot")
 
 #Create Indivudal maps
 s1<-tm_shape(export) +
-  tm_polygons("TC Samples", palette = 'GnBu', style = 'fixed', breaks=c(0,5,10,50,200,4300)) +
-  tm_layout(frame=F, legend.show = T, title="E)", title.size = 14/12) 
+  tm_polygons("TC Samples", palette = 'YlOrBr', style = 'fixed', breaks=c(0,5,10,50,200,4300)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="E)", title.size = 14/12) 
 s2<-tm_shape(export) +
-  tm_polygons("EC Samples", palette = 'RdPu', style = 'fixed', breaks=c(0,5,10,50,100,1500))+
-  tm_layout(frame=F, legend.show = T, title="F)", title.size = 14/12)  
+  tm_polygons("EC Samples", palette = 'Greens', style = 'fixed', breaks=c(0,5,10,50,100,1500))+
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="F)", title.size = 14/12)  
 s3<-prop_pos_TC<-tm_shape(export) +
-  tm_polygons('TC Pos (%)', palette = "YlOrBr", style = 'fixed', breaks=c(0,10,25,50,75,100)) +
-  tm_layout(frame=F, legend.show = T, title="G)", title.size = 14/12) 
+  tm_polygons('TC Pos (%)', palette = "Reds", style = 'fixed', breaks=c(0,10,25,50,75,100)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="G)", title.size = 14/12) 
 s4<-tm_shape(export) + 
-  tm_polygons('EC Pos (%)', palette = 'Greens', style = 'fixed', breaks=c(0,10,25,50,75,100)) +
-  tm_layout(frame=F, legend.show = T, title="H)", title.size = 14/12)     
+  tm_polygons('EC Pos (%)', palette = 'YlGn', style = 'fixed', breaks=c(0,10,25,50,75,100)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="H)", title.size = 14/12)     
 
 #Print
 tmap_arrange(s1,s2,s3,s4,ncol=2)
@@ -402,8 +436,6 @@ tmap_mode("plot")
 tmap_arrange(w1, w2, s1,s2, w3, w4, s3,s4,ncol=4)
 dev.off()
 
-
-
 #5.4 Rural vs city plots-------------------------------------------------------_
 #Intall packages
 library(ggpubr)
@@ -415,18 +447,17 @@ m<-counties %>%
   #Drop geometry
   st_drop_geometry() %>% 
   #select collumns of interest
-  dplyr::select(cdc_code, inun_well_users, prop_inun_wells) %>% 
+  dplyr::select(cdc_code, inun_well_users, total_well_users) %>% 
   #Urban/rural classfication
   mutate(cdc_code = if_else(cdc_code>4, 'Rural', 'Urban')) %>% 
-  #Convert to %
-  mutate(prop_inun_wells = prop_inun_wells*100) %>% as_tibble() %>% 
+  as_tibble() %>% 
   #Change cdc_code to County Type for legend
   rename('County Type' = 'cdc_code') 
 
 m1<-ggscatterhist(m,
   #Scatter plot Data
   x = 'inun_well_users', 
-  y = 'prop_inun_wells', 
+  y = 'total_well_users', 
   color = "County Type",
   #font sizes
   font.x = c(14), 
@@ -436,7 +467,7 @@ m1<-ggscatterhist(m,
   #Scatterplot options
   palette = c('#E57200','#232D4B'),
   xlab = "Total well users per county",
-  ylab = "Wells users impacted\nby inundation [%]",
+  ylab = "Total well users\nimpacted by inundation",
   size = 6, alpha = 0.7,
   #histogram options
   margin.plot.size = 1.75,
@@ -538,3 +569,162 @@ tot_pop<-m %>% filter(cdc_code == 'total', name=='total_population') %>% select(
 tot_well<-m %>% filter(cdc_code=='total', name=='total_well_users') %>% select(value) %>% pull()
 1-tot_well/tot_pop
 
+#5.6 SI Figures ----------------------------------------------------------------
+#sampled data ------------------------------------------------------------------
+#Convert df_zip results into wider format
+export<-df_zip %>% 
+  #Pivot longer
+  pivot_longer(
+    -c(zip, test)
+  ) %>% 
+  #combine test/name cols
+  mutate(name = paste0(test,"_", name)) %>% 
+  select(-test) %>% 
+  #Pivot wider
+  pivot_wider(names_from = name, 
+              values_from = value) %>% 
+  #Estimate percent pos
+  select(
+    'TC Pos (%)' = TC_n_pos/TC_n_samples, 
+    'EC Pos (%)' = EC_n_pos/EC_n_samples,
+    'TC Samples' = TC_n_samples,
+    'EC Samples' = EC_n_samples)
+
+#Export csv 
+write_csv(export, paste0(output_dir,'zip_sampling.csv'))
+
+#Joint to spatial data
+export<-zip_shp %>% 
+  #Convert zip col to double
+  mutate(zip = as.numeric(zip)) %>% 
+  #join to df_zip with sampling data
+  left_join(., export)
+
+#Plot
+#Turn plotting device on
+png(paste0(output_dir, "zip_testing_map.png"), width=7,height=6, units = "in", res=300)
+tmap_mode("plot")
+
+#Create Indivudal maps
+s1<-tm_shape(export) +
+  tm_polygons("TC Samples", palette = 'YlOrBr', style = 'fixed', breaks=c(0,5,10,50,200,4300)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="E)", title.size = 14/12) 
+s2<-tm_shape(export) +
+  tm_polygons("EC Samples", palette = 'Greens', style = 'fixed', breaks=c(0,5,10,50,100,1500))+
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="F)", title.size = 14/12)  
+s3<-prop_pos_TC<-tm_shape(export) +
+  tm_polygons('TC Pos (%)', palette = "Reds", style = 'fixed', breaks=c(0,10,25,50,75,100)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="G)", title.size = 14/12) 
+s4<-tm_shape(export) + 
+  tm_polygons('EC Pos (%)', palette = 'YlGn', style = 'fixed', breaks=c(0,10,25,50,75,100)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="H)", title.size = 14/12)     
+
+#Print
+tmap_arrange(s1,s2,s3,s4,ncol=2)
+
+#Turn printing device off
+dev.off()
+
+#modeled data-------------------------------------------------------------------
+#Project zip_shp
+zip_shp<-st_transform(zip_shp, crs = st_crs(p))
+
+#Create function to sum by zip
+fun<-function(n){
+  
+  #Select county
+  zip_shp<-zip_shp[n,]
+  
+  x<-tryCatch(crop(wells, zip_shp), error = function(e) 1)
+  
+  if(is(x)[2]=='Raster'){
+    #crop inundation and wells to counties
+    wells      <- crop(wells,      zip_shp)
+    inundation <- crop(inundation, zip_shp)
+    wells      <- mask(wells,      zip_shp)
+    inundation <- mask(inundation, zip_shp)
+    
+    #Create output tibble
+    output<-tibble(
+      NAME = zip_shp$zip,
+      total_area = st_area(zip_shp), 
+      inun_area  = cellStats(inundation, sum)*(res(inundation)[1]^2),
+      prop_inun_area = inun_area/total_area,
+      total_well_users = cellStats(wells, sum),
+      inun_well_users  = cellStats(wells*inundation, sum), 
+      prop_inun_wells  = inun_well_users/total_well_users)
+  }else{
+    #if Extents don't overlap
+    output<-NA
+  }
+  
+  #Export
+  output
+}
+
+#apply function
+output<-lapply(seq(1, nrow(zip_shp)), fun) %>% bind_rows()
+output<-output %>% mutate(zip = NAME)
+
+#Left Join to counties sf
+zip_shp<-zip_shp %>% dplyr::left_join(., output)
+zip_shp <- zip_shp %>% 
+  mutate(
+    total_area = as.numeric(paste(total_area)), 
+    prop_inun_area = as.numeric(paste(prop_inun_area))
+  )
+
+#Define cols for plotting
+zip_shp<-zip_shp %>% 
+  mutate(
+     zip,
+    'Well Users' = total_well_users,
+    'Area flooded (%)' = inun_area/total_area*100, 
+    'Well users flooded' = inun_well_users,
+    'Well users flooded (%)' = inun_well_users/total_well_users*100
+  ) %>% 
+  select(zip, 'Well Users','Area flooded (%)',
+         'Well users flooded','Well users flooded (%)')
+
+#Start plot
+png(paste0(output_dir, "zip_well_map.png"), width=7,height=6, units = "in", res=300)
+tmap_mode("plot")
+
+#Create plots
+w1<-tm_shape(zip_shp) + 
+  tm_polygons('Well Users', palette = "RdPu", style = 'fixed', breaks=c(0,100,500,1500,15000)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="A)", title.size = 14/12)  
+w2<-tm_shape(zip_shp) + 
+  tm_polygons('Area flooded (%)', palette = "GnBu", style = 'fixed', breaks=c(0,1,5,10,25,100)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="B)", title.size = 14/12)  
+w3<-tm_shape(zip_shp) + 
+  tm_polygons('Well users flooded', palette = "BuPu",style = 'fixed', breaks=c(0,5,10,15, 20, 1200)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="C)", title.size = 14/12)  
+w4<-tm_shape(zip_shp) + 
+  tm_polygons('Well users flooded (%)', palette = "Blues", style = 'fixed', breaks=c(0,1,2, 5,10,20)) +
+  tm_layout(frame=F, 
+            legend.show = T, legend.text.size = 8/12, 
+            title="D)", title.size = 14/12) 
+
+#plot
+tmap_arrange(w1,w2,w3,w4, ncol=2)
+
+#Turn device off
+dev.off()
+
+#Export csv
+zip_shp %>% st_drop_geometry() %>% write_csv(., paste0(output_dir,"zip_wells.csv"))
